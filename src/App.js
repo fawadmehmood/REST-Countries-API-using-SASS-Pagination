b8513af0b0fa6dbf0c1b1a203b820e4070529829
fawadmehmood <fawadmehmood10@gmail.com>
2022-09-12 16:02:56 +0500

    added dark/light theme on button click

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import "./dist/css/styles.css";
 
 function App() {
+  console.log("rerender App");
   return (
     <div className="App">
       <Header />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,28 @@ import React, { useState, useEffect } from "react";
 import "../dist/css/header.css";
 
 const Header = () => {
+  const getUserTheme = () => {
+    const theme = localStorage.getItem("theme") || "light";
+    return theme;
+  };
+  const [theme, setTheme] = useState(getUserTheme());
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+  }, [theme]);
+
+  const switchTheme = (themePref) => {
+    localStorage.setItem("theme", themePref);
+    setTheme(themePref);
+  };
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      return switchTheme("dark");
+    }
+    switchTheme("light");
+  };
+
   return (
     <header>
       <div className="headerWrapper container">
