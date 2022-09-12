d37ad8977f9d09fff9e77318c90acc5c37c03bb2
fawadmehmood <fawadmehmood10@gmail.com>
2022-09-12 21:45:06 +0500

    added placeholders for repeating styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import ThemeSwitcher from "./ThemeSwitcher";
 const Header = () => {
   return (
     <header>
-      <div className="headerWrapper container">
+      <div className="headerWrapper">
         <h1>Where in the world ?</h1>
         <ThemeSwitcher />
       </div>
